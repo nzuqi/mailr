b5027a0d7ed62e377f8f97de33f18818918e40d2
Martin Nzuki <iamnzuki@gmail.com>
2025-08-15 15:28:29 +0300

    Queue messages

--- src/routes/message.route.ts
+++ src/routes/message.route.ts
@@ -5,7 +5,7 @@ import { authenticate } from '../utils';
 export const messageRoutes = () => {
   const router = Router();
 
-  router.post('/v1/messages', authenticate(), queueMessage);
+  router.post('/v1/messages', queueMessage);
 
   router.get('/v1/messages', authenticate(), getAllMessages);
 
