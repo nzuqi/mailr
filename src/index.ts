280da7ec3ce25c87f9a54c5ea6f4f7ec28e8630d
Martin Nzuki <iamnzuki@gmail.com>
2025-08-15 10:08:05 +0300

    Service .env fixes

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import { connectToDatabase } from './db-connection';
 import { errorHandler } from './utils/middleware/error-handler';
 import routes from './routes';
 import { logger, startMessageProcessor } from './utils';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 
 const HOST = process.env.HOST || 'http://localhost';
 const PORT = parseInt(process.env.PORT || '4500');
